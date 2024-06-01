106dbf27c2a94ba523b4aefa75b36c5d4b569ca4
anhtonthattuan <a.ton.that.tuan.anh@student.tue.nl>
2024-06-01 15:48:42 +0200

    Update details are in whatsapp

--- src/power_system_simulation/power_grid_model.py
+++ src/power_system_simulation/power_grid_model.py
@@ -1,5 +1,47 @@
-from typing import List, Tuple
-from power_system_simulation.graph_processing import GraphProcessor
+# import 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import json
 
-class ValidationException(Exception):
+from power_grid_model import (
+    LoadGenType,
+    PowerGridModel,
+    CalculationType,
+    CalculationMethod,
+    initialize_array
+)
+
+from power_grid_model.validation import (
+    assert_valid_input_data,
+    assert_valid_batch_data
+)
+
+from power_grid_model.utils import (
+    json_deserialize, 
+    json_serialize
+)
+
+class ProfilesNotMatching(Exception):
+    pass
+
+def powerGridModelling(
+        data_path: str,
+        active_sym_load_path: str,
+        reactive_sym_load_path: str) -> None:
+    with open(data_path) as fp0:
+        data = fp0.read()
+    dataset = json_deserialize(data)
+    assert_valid_input_data(input_data= dataset, calculation_type= CalculationType.power_flow)
+    model = PowerGridModel(dataset)
+    active_load_profile = pd.read_parquet(active_sym_load_path)
+    reactive_load_profile = pd.read_parquet(reactive_sym_load_path)
+    if not active_load_profile.index.equals(reactive_load_profile.index) or not active_load_profile.columns.equals(reactive_load_profile.columns):
+        raise ProfilesNotMatching
+    load_profile = initialize_array("update", "sym_load", active_load_profile)
+    load_profile["id"] = active_load_profile.columns.to_numpy()
+    load_profile["p_specified"] = active_load_profile.to_numpy()
+    load_profile["q_specified"] = reactive_load_profile.to_numpy()
+    update_dataset = {"sym_load": load_profile}
+    assert_valid_batch_data(input_data= dataset, update_data= update_dataset, calculation_type= CalculationType.power_flow)
     pass
---
+++ tests/test_power_grid_model/test_power_grid.py
@@ -0,0 +1,14 @@
+"""
+Copy test data path
+"""
+data_path = "tests/test_power_grid_model/input_network_data.json"
+active_sym_load_path = "tests/test_power_grid_model/active_power_profile.parquet"
+reactive_sym_load_path = "tests/test_power_grid_model/reactive_power_profile.parquet"
+
+"""
+For error handling tests, you can create your own dataset instead of the one provided to us above. Make test datasets guide is in the power grid
+workshop examples 
+"""
+from power_system_simulation.power_grid_model import powerGridModelling
+
+powerGridModelling(data_path= data_path, active_sym_load_path= active_sym_load_path, reactive_sym_load_path= reactive_sym_load_path)
